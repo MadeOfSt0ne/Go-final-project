2d48522ae7beaa54e839db8fae7f1ba0135b4a79
Artur M. <artyrillo@yandex.ru>
2024-04-01 19:50:18 +0300

    feat file server

---
+++ cmd/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log/slog"
+	"net/http"
+	"os"
+
+	"github.com/lpernett/godotenv"
+)
+
+const (
+	webDir   = "../web/"
+	portName = "TODO_PORT"
+)
+
+func main() {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		slog.Error("can't load .env file")
+		os.Exit(1)
+	}
+
+	mux := http.NewServeMux()
+	//mux.Handle("/", http.StripPrefix("/web", http.FileServer(http.Dir(webDir))))
+	mux.Handle("/", http.FileServer(http.Dir(webDir)))
+
+	var port string
+	port, ok := os.LookupEnv(portName)
+	if !ok {
+		slog.Info("can't find port in .env", "port", portName)
+		port = ":7540"
+	}
+
+	err = http.ListenAndServe(port, mux)
+	if err != nil {
+		slog.Error("failed to listen and serve", "error", err)
+		os.Exit(1)
+	}
+}
